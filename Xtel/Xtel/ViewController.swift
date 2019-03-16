4ad70d4b52915623bbddec120c8a6db590d93816
Codesworth <codezworth@gmail.com>
2019-03-16 23:58:54 +0000

    Basic setup with Command Queues

--- Xtel/Xtel/ViewController.swift
+++ Xtel/Xtel/ViewController.swift
@@ -7,12 +7,38 @@
 //
 
 import UIKit
+import MetalKit
+
+
 
 class ViewController: UIViewController {
+    
+    enum Colors{
+        static let ray = MTLClearColor(red: 0, green: 0.4, blue: 0.21, alpha: 1)
+    }
+    
+    var metalView:MTKView{
+        return view as! MTKView
+    }
 
+    var device:MTLDevice!
+    var commandQueue:MTLCommandQueue!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        metalView.device = MTLCreateSystemDefaultDevice()
+        device = metalView.device
+        
+        metalView.clearColor = Colors.ray
+        
+        commandQueue = device.makeCommandQueue()
+        let c_buff = commandQueue.makeCommandBuffer()
+        
+        let c_encoder = c_buff?.makeRenderCommandEncoder(descriptor: metalView.currentRenderPassDescriptor!)
+        c_encoder?.endEncoding()
+        c_buff?.present(metalView.currentDrawable!)
+        c_buff?.commit()
     }
 
 

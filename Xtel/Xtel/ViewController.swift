218bb0dbfea25d1a094eb0e6967b10e0dbec7826
Codesworth <codezworth@gmail.com>
2019-03-16 23:43:46 +0000

    Initialized With First Project

---
+++ Xtel/Xtel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xtel
+//
+//  Created by Shadrach Mensah on 16/03/2019.
+//  Copyright © 2019 Shadrach Mensah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
